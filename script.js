d1f2b21ef05bcdf9ad95a9d31a9e15bf3b9978f4
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-06 15:33:11 +0100

    Check whether all the computer choices are equally likely to occur

--- script.js
+++ script.js
@@ -5,5 +5,39 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function testComputerChoice(num) {
+    let rockCounter = 0;
+    let paperCounter = 0;
+    let scissorsCounter = 0;
+
+    console.group("Simulation");
+    console.log("Simulating computer choices...")
+    for (let i = 0; i < num; i++) {
+        let computerChoice = getComputerChoice();
+
+        switch (computerChoice) {
+            case 'Rock':
+                rockCounter++;
+            break;
+
+            case 'Paper':
+                paperCounter++;
+            break;
+
+            case 'Scissors':
+                scissorsCounter++;
+            break;
+        }
+    }
+
+    console.log(`Out of ${num} simulations computer chose:`);
+    console.log(`Rock ${rockCounter} times.`);
+    console.log(`Paper ${paperCounter} times.`);
+    console.log(`Scissors ${scissorsCounter} times.`);
+    console.groupEnd();
+}
+
+testComputerChoice(100);
+
 let computerSelection = getComputerChoice();
-console.log(`Computer chose ${computerChoice}.`);
+console.log(`Computer chose ${computerSelection}.`);
